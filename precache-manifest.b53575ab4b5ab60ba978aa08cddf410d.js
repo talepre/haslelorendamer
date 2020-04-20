c0e09c650ff95226adf1196372ba09b0cf82329d
talepre <tale.prestmo@gmail.com>
2020-04-20 13:51:05 +0200

    Updates

---
+++ precache-manifest.b53575ab4b5ab60ba978aa08cddf410d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcfdd6823dc8be36e59d575d9ee54a3",
+    "url": "/haslelorendamer/index.html"
+  },
+  {
+    "revision": "1bf95416746a0e2e293c",
+    "url": "/haslelorendamer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "45998c7a5443e251c84f",
+    "url": "/haslelorendamer/static/js/2.bf37ea7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haslelorendamer/static/js/2.bf37ea7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf95416746a0e2e293c",
+    "url": "/haslelorendamer/static/js/main.e5846e76.chunk.js"
+  },
+  {
+    "revision": "937132163bb23164d118",
+    "url": "/haslelorendamer/static/js/runtime-main.e2bf2c5f.js"
+  }
+]);
