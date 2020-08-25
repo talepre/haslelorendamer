d7447d8e83043dc8d6ddaf095bf1f082ca1c3000
talepre <tale.prestmo@gmail.com>
2020-08-25 22:39:17 +0200

    Fix background as pleased

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {ReactComponent as Logo} from './undraw_junior_soccer_6sop.svg';
 
 const Home: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hasle Løren Damer - Fotball
+    <div className="Home">
+      <header className="Home-header">
+        Hasle Løren Damer
       </header>
+      <Logo className="Home-logo" />
     </div>
   );
 }
